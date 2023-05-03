21b82fb2237ac64f85f1e5aaab60fa035b1ee718
Ren Masahiro <29018052+Ren112358@users.noreply.github.com>
2023-05-04 02:07:03 +0900

    [add] read csv func.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::fs::File;
+use std::path::Path;
+
+use polars::prelude::*;
+use polars::frame::DataFrame;
+use polars::prelude::Result as PolarResult;
+
+fn read_csv<P: AsRef<Path>>(path: P) -> PolarResult<DataFrame> {
+    // The schema below is specific to palmerpenguins dataset
+    // URL: https://allisonhorst.github.io/palmerpenguins/
+    let schema = Schema::new(vec![
+        Field::new("species", DataType::Utf8),
+        Field::new("island", DataType::Utf8),
+        Field::new("culmen_length_mm", DataType::Float64),
+        Field::new("culmen_depth_mm", DataType::Float64),
+        Field::new("flipper_length_mm", DataType::Float64),
+        Field::new("body_mass_g", DataType::Float64),
+        Field::new("sex", DataType::Utf8)
+    ]);
+
+    let file = File::open(path).expect("Cannot open file.");
+    CsvReader::new(file)
+        .with_schema(Arc::new(schema))
+        .has_header(true)
+        .with_ignore_parser_errors(true)
+        .finish()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "../data/palmerpenguins.csv";
+    let df: DataFrame = read_csv(file_path).unwrap();
+    println!("{:?}", df);
 }
